83b4b58292a4bdb58511540570ea6f0aaafeb92e
kelsy_vaughn <vaughnkelsy@gmail.com>
2026-04-26 13:34:53 -0700

    added script

---
+++ kvaughn_hour10/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,18 @@
+/* 
+Game Critic
+
+Is the Game too hard/easy?
+-- it's not too hard but the challenge is a bit boring
+
+What would make the game easier/harder?
+
+-- adding stationary elements that the player can move around in order to help guide the balls into a goal 
+ 
+What would give a "wow" factor?
+-- some special effects when the player scores or the balls exploding. 
+
+what parts of the game are fun or tedious? 
+
+-- navigating the block around the game.    
+
+*/
