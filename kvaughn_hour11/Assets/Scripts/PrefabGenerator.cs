4f4af57220b84a8d596e5fe11f095a8e7ef7db6a
kelsy_vaughn <vaughnkelsy@gmail.com>
2026-05-03 11:08:01 -0700

    updatePrefab

--- kvaughn_hour11/Assets/Scripts/PrefabGenerator.cs
+++ kvaughn_hour11/Assets/Scripts/PrefabGenerator.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    int count = 0;
+    private float distance = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       // AddLamps(count);
+       while (count < 10)
+       {
+           Vector3 offset = new Vector3(count, distance, count);
+           Instantiate(prefab, transform.position + offset, Quaternion.identity);
+           count += 1;
+       }
     }
 
     // Update is called once per frame
@@ -17,4 +26,6 @@ public class PrefabGenerator : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.B))Instantiate(prefab);
         if (Input.GetKeyDown(KeyCode.Space))Instantiate(prefab, transform.position, transform.rotation);
     }
+
+
 }
